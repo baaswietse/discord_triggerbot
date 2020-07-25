70b022d907cae4ca1229eb2668a82ea5b55e0e2d
unknown <wietse.delclef@gmail.com>
2020-07-25 12:51:45 +0200

    Regex voor commando validatie + lowercase + linter

--- src/commands.js
+++ src/commands.js
@@ -7,90 +7,80 @@ const returnTriggersCommand = 'triggerstats'
 const commands = {}
 
 commands.addTrigger = async function (message){
-    if(message.content.substring(0, message.content.indexOf(' ')) == addTriggerCommand){
-        const mentionsData = message.mentions.users.array()
-        // There are one or more users @mentions
-        if(mentionsData.length > 0){
-        const mentions = mentionsData.map(el=>{
-            const user = {
-            user_id: el.id,
-            username: el.username,
-            mentionedBy: message.author.id
-            }
-            return user
+  if(message.content.replace(/[ ].*$/, '').toLowerCase() == addTriggerCommand){
+    const mentionsData = message.mentions.users.array()
+    // There are one or more users @mentions
+    if(mentionsData.length > 0){
+      const mentions = mentionsData.map(el=>{
+        const user = {
+          user_id: el.id,
+          username: el.username,
+          mentionedBy: message.author.id
+        }
+        return user
+      })
+      console.log(mentions)
+
+      try{
+        // all @ mentioned users
+        await asyncForEach(mentions, async mention =>{
+          const user = await Users.findOne({user_id: mentions[0].user_id})
+          // user bestaat nog niet => aanmaken
+          if(user === null){
+            const newUser = await Users.create(mentions[0])
+            newUser.triggers.push({date: new Date(), mentionedBy: mentions[0].mentionedBy})
+            await newUser.save()
+          }else{
+            user.triggers.push({date: new Date(), mentionedBy: mentions[0].mentionedBy})
+            await user.save()
+          }
+
+          const userTriggers = await getNumberOfTriggers(mention.user_id)
+          message.channel.send(`<@${mention.user_id}> has been triggered ${userTriggers} times`)
         })
-        console.log(mentions)
+      }catch(err){
+        console.log(err)
+      }
 
-        try{
-            // all @ mentioned users
-            await asyncForEach(mentions, async mention =>{
-            const user = await Users.findOne({user_id: mentions[0].user_id})
-            // user bestaat nog niet => aanmaken
-            if(user === null){
-                const newUser = await Users.create(mentions[0])
-                newUser.triggers.push({date: new Date(), mentionedBy: mentions[0].mentionedBy})
-                await newUser.save()
-            }else{
-                user.triggers.push({date: new Date(), mentionedBy: mentions[0].mentionedBy})
-                await user.save()
-            }
-
-            const userTriggers = await getNumberOfTriggers(mention.user_id)
-            message.channel.send(`<@${mention.user_id}> has been triggered ${userTriggers} times`)
-            })
-        }catch(err){
-            console.log(err)
-        }
-
-        // No @ mentions
-        }else{
-        await message.channel.send('no "@" mention')
-        }
-    }   
+      // No @ mentions
+    }else{
+      await message.channel.send('no "@" mention')
+    }
+  }   
 }
   
 commands.returnTriggers = async function(message){
-    if(message.content.substring(0, message.content.indexOf(' ')) == returnTriggersCommand){
-        const mentionsData = message.mentions.users.array()
-        if(mentionsData.length > 0){
-        const userTriggers = await getNumberOfTriggers(mentionsData[0].id)
-        message.channel.send(`<@${mentionsData[0].id}> has been triggered ${userTriggers} times`)
+  if(message.content.replace(/[ ].*$/, '').toLowerCase() == returnTriggersCommand){
+    const mentionsData = message.mentions.users.array()
+    if(mentionsData.length > 0){
+      const userTriggers = await getNumberOfTriggers(mentionsData[0].id)
+      message.channel.send(`<@${mentionsData[0].id}> has been triggered ${userTriggers} times`)
 
-        // No @ mentions
-        }else{
-        await message.channel.send('no "@" mention')
-        }
+      // No @ mentions
+    }else{
+      await message.channel.send('no "@" mention')
     }
+  }
 }
   
 commands.listAllCommands = async function(message){
-    if(message.content == 'triggercommands' || message.content == 'triggerhelp'){
-        const commands = `A list of all supported commands:
+  if(message.content.toLowerCase() == 'triggercommands' || message.content == 'triggerhelp'){
+    const commands = `A list of all supported commands:
         > **trigger** *@user: triggers a user*
         > **triggerstats** *@user: gets the triggered stats of a user*
         `
-        await message.channel.send(commands)
-    }
+    await message.channel.send(commands)
+  }
 }
   
-/*commands.unknownCommands = async function(message){
-    if(message.content.startsWith('#trigger')){
-        const commands = `A list of all supported commands:
-        > **#trigger** *@user: triggers a user*
-        > **#triggerstats** *@user: gets the triggered stats of a user*
-        `
-        await message.channel.send(commands)
-    }
-}*/
-  
 async function getNumberOfTriggers(user_id){
-    try{
-      const user = await Users.findOne({user_id})
-      if(user) return user.triggers.length
-      return 0
-    }catch(err){
-      console.log(err)
-    }
+  try{
+    const user = await Users.findOne({user_id})
+    if(user) return user.triggers.length
+    return 0
+  }catch(err){
+    console.log(err)
   }
+}
 
 module.exports = commands
--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 require('dotenv').config()
-const Discord = require('discord.js');
-const mongoose = require("mongoose");
-const Users = require('./models/Users')
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const mongoose = require('mongoose')
+
 const commands  = require('./commands')
 
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
-connection.once("open", function() {
-  console.log("MongoDB database connection established successfully");
-});
+const client = new Discord.Client()
+
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+const connection = mongoose.connection
+connection.once('open', function() {
+  console.log('MongoDB database connection established successfully')
+})
 
 client.once('ready', () => {
-  console.log('Bot is ready');
-});
+  console.log('Bot is ready')
+})
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN)
 
 client.on('message', async (message) => {
-  if(message.author.bot) return;
+  // negeer alle bot commands
+  if(message.author.bot) return
 
   //loop over alle commands en invoke elke functies met de message
   for (var key in commands) {
-      if (commands.hasOwnProperty(key)) {
-          commands[key](message)
-      }
+    if (Object.prototype.hasOwnProperty.call(commands, key)) {
+      commands[key](message)
+    }
   }
-});
+})

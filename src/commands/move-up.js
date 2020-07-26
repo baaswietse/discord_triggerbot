7e223b261eb6f6cbbacf2df4b8dc0c45a38055ac
unknown <wietse.delclef@gmail.com>
2020-07-26 19:42:42 +0200

    emoji fix for move-up

--- src/commands/move-up.js
+++ src/commands/move-up.js
@@ -5,7 +5,7 @@ module.exports = {
 	execute(message, args) {
         let moveup = 'Got you fam\n'
         for (i = 0; i < 50; i++) {
-            moveup += ':kevinpride:\n'
+            moveup += ':eyes:\n'
           }
           message.channel.send(moveup)
 	},

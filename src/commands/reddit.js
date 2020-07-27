7a42ae8e6c689de62cfd5fd0c244d6eaef41142a
unknown <wietse.delclef@gmail.com>
2020-07-27 14:48:11 +0200

    Discord command => resize media if too big

--- src/commands/reddit.js
+++ src/commands/reddit.js
@@ -1,6 +1,17 @@
 const Discord = require('discord.js')
 const redditPost = require('../utils/redditRandomPostMedia')
+const nfetch = require('node-fetch')
+const gifResize = require('@gumlet/gif-resize')
 
+const {
+    promisify
+} = require('util')
+const {
+    writeFile,
+    readFileSync,
+    unlinkSync
+} = require('fs')
+const writeFilePromise = promisify(writeFile)
 
 module.exports = {
     name: 'reddit',
@@ -10,19 +21,53 @@ module.exports = {
     cooldown: 0,
     async execute(message, args) {
         if (args.length) {
+            let mediaUrl = ''
             if (args[0] == 'surprise') {
                 const subreddits = ['nsfw', 'amateur', 'amateurcumsluts', 'amateurporn', 'anal', 'besthqporngifs', 'boobbounce', 'collegesluts', 'drunkgirls', 'girlsdoingstuffnaked', 'gonewild', 'godpussy', 'homemadensfw', 'hotfallingdevilz', 'lanarhoades', 'lipsthatgrip', 'nsfw_gifs', 'pussy', 'unexpectedtitty', 'womenbendingover']
                 args[0] = subreddits[Math.floor(Math.random() * subreddits.length)]
             }
             try {
-                const media = await redditPost(args)
-                message.channel.send(`r/${args[0]}`, {
-                    files: [media],
+                mediaUrl = await redditPost(args)
+                await message.channel.send(`r/${args[0]}`, {
+                    files: [mediaUrl],
                     dynamic: true
                 })
             } catch (error) {
-                message.channel.send(error.message)
+                // gif downloaden en herschalen wanneer bestand te groot
+                if (error.message = 'Request entity too large') {
+                    message.channel.send('... resizing gif')
+                    resizeGif(message, mediaUrl, `r/${args[0]}`)
+                } else {
+                    message.channel.send(error.message)
+                }
             }
         }
+    }
+}
+
+async function resizeGif(message, mediaUrl, sub) {
+    const filename = /([^\/]+$)/.exec(mediaUrl)[0]
+    try {
+        nfetch(mediaUrl)
+            .then(x => x.arrayBuffer())
+            .then(x => writeFilePromise(filename, Buffer.from(x)))
+            .then(() => {
+                const buf = readFileSync(filename)
+                gifResize({
+                        width: 400
+                    })(buf).then(data => {
+                        writeFilePromise('output_' + filename, Buffer.from(data))
+                    })
+                    .then(async () => {
+                        await message.channel.send(sub, {
+                            files: ['./output_' + filename]
+                        })
+                    }).then(() => {
+                        unlinkSync(filename)
+                        unlinkSync('./output_' + filename)
+                    })
+            })
+    } catch (e) {
+        message.channel.send('e.message')
     }
 }
--- src/utils/redditRandomPostMedia.js
+++ src/utils/redditRandomPostMedia.js
@@ -1,24 +1,36 @@
+const util = require('util')
 const nfetch = require('node-fetch')
+const requestImageSize = require('request-image-size')
+const fs = require('fs')
+//const requestImageSizeAsync = util.promisify(requestImageSize)
 
 async function redditPostToEmbed(args) {
     return new Promise(async (resolve, reject) => {
-        nfetch(`https://www.reddit.com/r/${args[0]}.json?sort=best&t=week&limit=100`).then(res => res.json()).then(body => {
+        nfetch(`https://www.reddit.com/r/${args[0]}.json?limit=100`).then(res => res.json()).then(async body => {
             if (!body.data) return reject(new Error('subreddit not found'))
             if (body.data.children.length === 0) return reject(new Error('subreddit not found'))
 
             let images = []
             const posts = body.data.children
+            //fs.writeFileSync('test.json', JSON.stringify(posts))
             console.log(posts.length)
             for (const post of posts) {
                 const text = post.data
                 const extension = ['.jpg', '.png', '.svg', '.gif']
 
-                if (extension.includes(text.url.slice(-4))) {
+                if (text.media && text.media.oembed.thumbnail_url) {
+                    if (extension.includes(text.media.oembed.thumbnail_url.slice(-4))) {
+                        images.push(text.media.oembed.thumbnail_url)
+                    }
+                } else if (extension.includes(text.url.slice(-4))) {
                     images.push(text.url)
                     console.log('text', text.url)
                 }
             }
+            console.log(images)
             var image = images[Math.floor(Math.random() * images.length)]
+            //const imageSize = await requestImageSize(image)
+            ///console.log(imageSize)
             resolve(image)
         })
     })
